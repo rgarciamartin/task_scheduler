fd0df6813800f9d63946676510a5399e788ce0bc
Ramón García <rgarciamartin98@gmail.com>
2023-11-22 22:02:53 +0100

    Refactorización servicio de autenticación por un nombre más adecuado

--- app/backend/users/tests/test_auth.py
+++ app/backend/users/tests/test_auth.py
@@ -1,4 +1,4 @@
-from backend.users.auth import authenticate_user
+from backend.users.auth import auth_token_for_user_credentials
 from backend.users.tests.utils import AuthTokenTestUtils, UserTestUtils
 from django.core.exceptions import PermissionDenied
 from django.test import TestCase
@@ -12,17 +12,17 @@ class AuthenticateUserTestCase(TestCase):
 
     def test_username_is_required(self):
         with self.assertRaisesMessage(AssertionError, "Username is required."):
-            authenticate_user(username=None, password="test123")
+            auth_token_for_user_credentials(username=None, password="test123")
 
     def test_password_is_required(self):
         with self.assertRaisesMessage(AssertionError, "Password is required."):
-            authenticate_user(username="test_user", password=None)
+            auth_token_for_user_credentials(username="test_user", password=None)
 
     def test_wrong_credentials_gets_permission_denied(self):
         with self.assertRaisesMessage(PermissionDenied, "Incorrect username or password supplied."):
-            authenticate_user(username="test_user", password="pass")
+            auth_token_for_user_credentials(username="test_user", password="pass")
 
     def test_authentication(self):
         auth_token = AuthTokenTestUtils.create_auth_token_for_user(user_id=self.user.id)
-        received_token = authenticate_user(username="test_user", password="test123")
+        received_token = auth_token_for_user_credentials(username="test_user", password="test123")
         self.assertEqual(auth_token.key, received_token)
