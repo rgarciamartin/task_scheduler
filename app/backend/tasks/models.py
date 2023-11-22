d74e52c30613153a0e97f5c31d8cd6056c50024e
Ramón García <rgarciamartin98@gmail.com>
2023-11-22 22:48:43 +0100

    Modelo Tarea y migraciones correspondientes

--- app/backend/tasks/models.py
+++ app/backend/tasks/models.py
@@ -1,3 +1,25 @@
-from django.db import models
+from uuid import uuid4
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Task(models.Model):
+    uuid = models.UUIDField(unique=True, default=uuid4, editable=False)
+    owner = models.ForeignKey(User, related_name="tasks", on_delete=models.PROTECT)
+    created = models.DateTimeField(auto_now_add=True, editable=False)
+    last_updated = models.DateTimeField(auto_now=True)
+    title = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
+
+    class StatusChoices(models.TextChoices):
+        TO_DO = "to_do", "To Do"
+        IN_PROGRESS = "in_progress", "In Progress"
+        COMPLETED = "completed", "Completed"
+        STAND_BY = "stand_by", "Stand By"
+
+    status = models.CharField(choices=StatusChoices.choices, max_length=20, default=StatusChoices.TO_DO)
+
+    class Meta:
+        verbose_name = "Task"
+        verbose_name_plural = "Tasks"
